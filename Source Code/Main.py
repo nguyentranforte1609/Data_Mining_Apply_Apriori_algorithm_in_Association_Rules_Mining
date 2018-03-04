75a2e348879dc52159d320b2bd98a6cba9dce388
Nguyen Tran <nguyen.tran.forte1609@mail.com>
2018-03-04 22:22:48 +0700

    Complete coding for Association Rules

--- Source Code/Main.py
+++ Source Code/Main.py
@@ -1,5 +1,5 @@
 from itertools import combinations
-
+from itertools import permutations
 
 def getInput(numOfTrans, tableOfTrans):
     # Get number of transactions
@@ -11,6 +11,7 @@ def getInput(numOfTrans, tableOfTrans):
     for i in range(numOfTrans):
         tempInput = input("Transaction T" + str(i) + ": ")
         tableOfTrans.append(set(tempInput.split(" ")))
+    return  numOfTrans, tableOfTrans
 
 def generateInitalItemDict(itemDict, tableOfTrans):
     for trans in tableOfTrans:
@@ -19,21 +20,19 @@ def generateInitalItemDict(itemDict, tableOfTrans):
                 itemDict[item] = 0
     return  itemDict
 
-
 def countItemFrequency(itemDict, tableOfTrans, numOfTrans):
     for key in itemDict:
         for trans in tableOfTrans:
             if set(key).issubset(trans):
                 itemDict[key] += 1
 
-
-def saveItemSet(resItemSets, minSupp, itemDict, numOfTrans):
+def saveItemSet(resItemSets, minSupp, itemDict, numOfTrans, resDict):
     for key in itemDict:
         if float(itemDict[key]/numOfTrans) >= minSupp:
             resItemSets.append(set(list(key)))
+            resDict[key] = itemDict[key]
         else:
             itemDict[key] = -1
-
 
 def generateCombinations(baseItemDict):
     listItemSets = []  # Contains itemsets that satisfy minsupp on previous level
@@ -43,14 +42,12 @@ def generateCombinations(baseItemDict):
     combinationSets = combinations(listItemSets, 2)
     return  combinationSets
 
-
 def generateSets(combinationSets):
     sets = set()
     for t in combinationSets:
         tempSet = t[0].union(t[1])
         sets.add(tempSet)
     return  sets
-
 
 def pruneItemsets(listItemSets, baseItemDict):
     tempSet = set()
@@ -62,7 +59,6 @@ def pruneItemsets(listItemSets, baseItemDict):
     for subset in tempSet:
         listItemSets.remove(subset)
 
-
 def regenerateItemDict(baseItemDict, level):
     newItemDict = {}
     combinationSets = generateCombinations(baseItemDict)
@@ -73,29 +69,70 @@ def regenerateItemDict(baseItemDict, level):
         newItemDict[tempKey] = 0
     return newItemDict
 
-def applyApriori(numOfTrans, minSupp, tableOfTrans, resItemSets):
+def applyApriori(numOfTrans, minSupp, tableOfTrans, resItemSets, resDict):
     level = 1
     baseItemDict = {}
     while(True):
         if (level == 1):
             baseItemDict = generateInitalItemDict(baseItemDict, tableOfTrans)
             countItemFrequency(baseItemDict, tableOfTrans, numOfTrans)
-            saveItemSet(resItemSets, minSupp, baseItemDict, numOfTrans)
+            saveItemSet(resItemSets, minSupp, baseItemDict, numOfTrans, resDict)
             # print(baseItemDict) DEBUG
         if (level > 1):
             crrItemDict = {}
             baseItemDict = regenerateItemDict(baseItemDict, level)
             countItemFrequency(baseItemDict, tableOfTrans, numOfTrans)
-            saveItemSet(resItemSets, minSupp, baseItemDict, numOfTrans)
+            saveItemSet(resItemSets, minSupp, baseItemDict, numOfTrans, resDict)
             # print(baseItemDict) DEBUG
         level += 1
         # Loop condition
         # There is no more itemset can be generated
         if (len(baseItemDict) <= 1): break #Temporary condition used for debugging
 
+
+def findSupportValue(valueSet, resDict, isTuple = False):
+    if isTuple == False:
+        targetKey = "".join(valueSet)
+        for key in resDict:
+            if targetKey == key:
+                return resDict[key]
+    else:
+        targetKey01 = "".join(valueSet[0]) + "".join(valueSet[1])
+        targetKey02 = "".join(valueSet[1]) + "".join(valueSet[0])
+        for key in resDict:
+            if targetKey01 == key:
+                return resDict[key]
+            if targetKey02 == key:
+                return  resDict[key]
+    return 0
+
+
+
+def applyAssociationRules(resItemSets, resDict, minConf, numOfTrans, resConf):
+    tempCombinationSets = permutations(resItemSets,2)
+    for t in tempCombinationSets: # t is a tuple
+        supp01 = float(findSupportValue(t[0], resDict)/numOfTrans)
+        supp02 = float(findSupportValue(t, resDict, True)/numOfTrans)
+        try:
+            if float(supp01/supp02) >= minConf and t not in resConf:
+                resConf.append(t)
+        except:
+            continue
+
 #
 #   Main Operations
 #
+def displayResult(resItemSets, resConf):
+    print("=============================")
+    print("List of itemsets: ")
+    for item in resItemSets:
+        print(item)
+    print("=============================")
+    print("List of strong association: {X} => {Y}")
+    for item in resConf:
+        print(item)
+
+
 def Main():
     # Init
     numOfTrans = 0
@@ -103,16 +140,63 @@ def Main():
     minConf = 0.5
     tableOfTrans = []   # List of set
     resItemSets = []    # List of sets
+    resDict = {}
+    resConf = []
 
-    # Get input
-    #getInput(numOfTrans, tableOfTrans)
-    numOfTrans = 4
-    tableOfTrans = [
-        {"A","B","C","D"},
-        {"B", "B", "A", "C"},
-        {"D", "E", "C", "D"},
-        {"A", "C", "D", "C"},
-    ]
-    applyApriori(numOfTrans, minSupp, tableOfTrans, resItemSets)
-    print(resItemSets)
+
+    numOfTrans, tableOfTrans = getInput(numOfTrans, tableOfTrans)
+    applyApriori(numOfTrans, minSupp, tableOfTrans, resItemSets, resDict)
+    applyAssociationRules(resItemSets,resDict, minConf, numOfTrans, resConf)
+    displayResult(resItemSets, resConf)
+
+# Start
 Main()
+
+""" 
+[SAMPLE_01]
+Input number of transactions: 4
+Transaction T0: A B C
+Transaction T1: A C
+Transaction T2: A D
+Transaction T3: B E F
+=============================
+List of itemsets: 
+{'A'}
+{'C'}
+{'B'}
+{'A', 'C'}
+=============================
+List of strong association: {X} => {Y}
+({'A'}, {'C'})
+({'C'}, {'A'})
+
+
+[SAMPLE_02]
+Input number of transactions: 4
+Transaction T0: A B C D
+Transaction T1: A A D E
+Transaction T2: B E F G
+Transaction T3: C A E F
+=============================
+List of itemsets: 
+{'B'}
+{'D'}
+{'C'}
+{'A'}
+{'E'}
+{'F'}
+{'E', 'A'}
+{'C', 'A'}
+{'E', 'F'}
+{'D', 'A'}
+=============================
+List of strong association: {X} => {Y}
+({'D'}, {'A'})
+({'C'}, {'A'})
+({'A'}, {'D'})
+({'A'}, {'C'})
+({'A'}, {'E'})
+({'E'}, {'A'})
+({'E'}, {'F'})
+({'F'}, {'E'})
+"""
